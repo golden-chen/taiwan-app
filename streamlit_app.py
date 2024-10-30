19a8f7363c3823ec9299da88f47ccc868687f63c
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-10-31 05:05:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Taiwan story")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start discovering! For wonderful and nice, head over to [Taiwan](https://tw.yahoo.com)."
 )
