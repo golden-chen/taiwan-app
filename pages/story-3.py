08b5af4f7671e8d380021e16fc8d93b79f5f826c
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-01 06:23:53 +0800

    Update story-3.py

--- pages/story-3.py
+++ pages/story-3.py
@@ -1,1 +1,6 @@
-
+import streamlit as st
+#[server]
+#enableStaticServing = true
+st.markdown("# Story-3 ❄️")
+st.sidebar.markdown("# Story-3 ❄️")
+st.image("https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80", caption="Sunrise by the mountains")
