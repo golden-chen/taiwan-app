175a5f4a45dd920dfad2d3d6b847e92c3ed40111
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-01 04:46:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+
+#st.markdown("# main ❄️")
+st.sidebar.markdown("# Taiwan story ❄️")
 st.title("🎈 Taiwan story")
 st.write(
     "Let's start discovering! For wonderful and nice, head over to [Taiwan](https://tw.yahoo.com)."
