09dd721ef5edfc988f6deef88f4ef6e6fa25c13c
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-01 06:14:38 +0800

    Update story-2.py

--- pages/story-2.py
+++ pages/story-2.py
@@ -1,6 +1,6 @@
 import streamlit as st
-[server]
-enableStaticServing = true
+#[server]
+#enableStaticServing = true
 st.markdown("# Story-2 ❄️")
 st.sidebar.markdown("# Story-2 ❄️")
 st.image("./app/static/b2.jpg")
