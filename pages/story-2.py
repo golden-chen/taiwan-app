7f6aec7e175ee764907045d7ff948fc27aa8c82e
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-02 04:39:01 +0800

    Update story-2.py

--- pages/story-2.py
+++ pages/story-2.py
@@ -1,5 +1,14 @@
 import streamlit as st
-"test",st.session_state
+st.session_state['Golden']='best'
+st.session_state['terry']='weak'
+st.session_state['length']=31
+st.session_state['SWITCH']=True
+"Before of test",st.session_state
 st.markdown("# Story-2 ❄️")
+st.session_state.Golden='The best'
+st.session_state['terry']='bad'
+st.session_state.length=39
+st.session_state['SWITCH']=False
+"After of test",st.session_state
 st.sidebar.markdown("# Story-2 ❄️")
 st.image("./static/b2.jpg", caption="Sunrise by the mountains")
