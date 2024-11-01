f4442bfb32684ee1d9f37b14c6830fab70093044
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-02 05:13:29 +0800

    Update story-1.py

--- pages/story-1.py
+++ pages/story-1.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+c1,c2,c3=st.beta_columns([3,1,2])
+c1.number_input('weight',key='w')
+c3.slider('Length',1,200,key='l')
+st.session_state
 st.markdown("# Story-1 ❄️")
 st.sidebar.markdown("# Stoory-1 ❄️")
 st.image("./static/b1.png", caption="Super star")
