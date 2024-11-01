f23637edd6d18d8acdcca8240c4d2cbc3b2e7440
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-01 23:02:01 +0800

    Update story-3.py

--- pages/story-3.py
+++ pages/story-3.py
@@ -3,6 +3,7 @@ import pandas as pd
 st.markdown("# Story-3 ❄️")
 st.sidebar.markdown("# Story-3 ❄️")
 st.image("https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80", caption="Sunrise by the mountains")
+@st.cache_data
 def load_data(url):
     df = pd.read_csv(url)  # 👈 Download the data
     return df
