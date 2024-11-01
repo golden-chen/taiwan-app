d6cc676f1ef1507c6bd68fb7c15cbfe30442c713
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-01 22:03:36 +0800

    Update story-3.py

--- pages/story-3.py
+++ pages/story-3.py
@@ -4,3 +4,11 @@ import streamlit as st
 st.markdown("# Story-3 ❄️")
 st.sidebar.markdown("# Story-3 ❄️")
 st.image("https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80", caption="Sunrise by the mountains")
+def load_data(url):
+    df = pd.read_csv(url)  # 👈 Download the data
+    return df
+
+df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")
+st.dataframe(df)
+
+st.button("Rerun")
