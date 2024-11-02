64c1d744a8653fcffcf40831ea8cd90c20695fbb
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-02 20:28:21 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -5,24 +5,24 @@ st.sidebar.markdown("# test ❄️")
 st.title('Counter Example using Callbacks')
 if 'count' not in st.session_state:
     st.session_state.count = 0
-
+c1,c2=st.columns([1,1])
 def increment_counter():
     st.session_state.count += 1
 
-st.button('Increment', on_click=increment_counter)
+c1.button('Increment', on_click=increment_counter)
 
-st.write('Count = ', st.session_state.count)
-st.markdown("#  ❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄")
+c2.write('Count = ', st.session_state.count)
+st.markdown("###  ❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄")
 st.title('Counter Example using Callbacks with args')
 if 'count1' not in st.session_state:
     st.session_state.count1 = 0
-
-increment_value = st.number_input('Enter a value', value=0, step=1)
+c3,c4,c5=st.colummns([1,1,1])
+increment_value = c3.number_input('Enter a value', value=0, step=1)
 
 def increment_counter1(increment_value):
     st.session_state.count1 += increment_value
 
-increment = st.button('Add', on_click=increment_counter1,
+increment = c4.button('Add', on_click=increment_counter1,
     args=(increment_value, ))
 
-st.write('Count = ', st.session_state.count1)
+c5.write('Count = ', st.session_state.count1)
