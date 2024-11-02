b2bafb0e02fd207fc0607250066501c9554450c5
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-02 21:28:22 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -16,13 +16,15 @@ st.divider()
 st.title('Counter Example using Callbacks with args')
 if 'count1' not in st.session_state:
     st.session_state.count1 = 0
-c3,c4,c5=st.columns([3,3,3])
+c3,c4,c5,c6=st.columns([3,2,2,2])
 increment_value = c3.number_input('Enter a value', value=0, step=1)
 
-def increment_counter1(increment_value):
+def add_counter1(increment_value):
     st.session_state.count1 += increment_value
-
-increment = c4.button('Add', on_click=increment_counter1,
+def sub_counter1(increment_value):
+    st.session_state.count1 -= increment_value
+increment = c4.button('Add', on_click=add_counter1,
     args=(increment_value, ))
-
-c5.text('Count1 = '+str(st.session_state.count1))
+increment = c5.button('Add', on_click=sub_counter1,
+    args=(increment_value, ))
+c6.text('Count1 = '+str(st.session_state.count1))
