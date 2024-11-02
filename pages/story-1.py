f15dcb6249b866c87282ca0095a038c0eaa3d9a2
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-03 03:18:25 +0800

    Update story-1.py

--- pages/story-1.py
+++ pages/story-1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 c1,c2,c3=st.columns([3,1,2])
 c1.number_input('weight', value=40, step=1,key='w')
-c3.slider('Length',1,200,key='l')
+c3.slider('Length', value=40,1,200,key='l')
 st.session_state
 st.markdown("# Story-1 ❄️")
 st.sidebar.markdown("# Stoory-1 ❄️")
