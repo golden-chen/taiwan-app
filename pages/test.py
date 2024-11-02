3be0da9876a8740e81b862103426feab2caf1226
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-02 20:21:35 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -22,7 +22,7 @@ increment_value = st.number_input('Enter a value', value=0, step=1)
 def increment_counter1(increment_value):
     st.session_state.count1 += increment_value
 
-increment = st.button('Increment', on_click=increment_counter1,
+increment = st.button('Add', on_click=increment_counter1,
     args=(increment_value, ))
 
 st.write('Count = ', st.session_state.count1)
