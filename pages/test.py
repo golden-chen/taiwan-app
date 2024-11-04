1fd3618623511be4e1043d3a3c37604cdfa132f6
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-04 19:08:34 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -50,13 +50,13 @@ with tab5:
 with tab6:
     st.header('test dialog')
     
-        @st.dialog("Cast your vote")
-        def vote(item):
-            st.write(f"Why is {item} your favorite?")
-            reason = st.text_input("Because...")
-            if st.button("Submit"):
-                st.session_state.vote = {"item": item, "reason": reason}
-                st.rerun()
+    @st.dialog("Cast your vote")
+    def vote(item):
+         st.write(f"Why is {item} your favorite?")
+         reason = st.text_input("Because...")
+         if st.button("Submit"):
+             st.session_state.vote = {"item": item, "reason": reason}
+             st.rerun()
         
         if "vote" not in st.session_state:
             st.write("Vote for your favorite")
