b9b82bf8a9a292a61c4e66876e13c9a902840161
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-04 19:32:06 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -49,24 +49,26 @@ with tab5:
 #-----------------------------------------------------
 with tab6:
     st.header('test dialog')
-    
-    @st.dialog("Cast your vote")
-    def vote(item):
-         st.write(f"Why is {item} your favorite?")
-         reason = st.text_input("Because...")
-         if st.button("Submit"):
-             st.session_state.vote = {"item": item, "reason": reason}
-             st.rerun()
-        
-    if "vote" not in st.session_state:
-        st.write("Vote for your favorite")
-        if st.button("A"):
-            vote("A")
-        if st.button("B"):
-            vote("B")
-    else:
-        f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
-    #st.button('dialog',on_click=tvote)       
+    @st.fragment
+    def fragment_function():
+        @st.dialog("Cast your vote")
+        def vote(item):
+             st.write(f"Why is {item} your favorite?")
+             reason = st.text_input("Because...")
+             if st.button("Submit"):
+                 st.session_state.vote = {"item": item, "reason": reason}
+                 st.rerun()
+            
+        if "vote" not in st.session_state:
+            st.write("Vote for your favorite")
+            if st.button("A"):
+                vote("A")
+            if st.button("B"):
+                vote("B")
+        else:
+            f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
+    #st.button('dialog',on_click=tvote) 
+    fragment_function()
 #--------------------------------------------------------    
 with tab7:
     st.header('form')
