854b86f5bb8d89724a3ad254c36ffea3a3a162f0
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-05 03:13:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write(
 st.write(
     "Let's test again!"
 )
-s1_pg = st.Page("pages/story-1.py", title="story-1", icon=":coffee:")
+s1_pg = st.Page("pages/story-1.py", title="story-1", icon=":material/coffee:")
 s2_pg = st.Page("pages/story-2.py", title="story-2", icon=":material/deer:")
 s3_pg = st.Page("pages/story-3.py", title="story-3", icon=":material/bat:")
 test_pg = st.Page("pages/test.py", title="test", icon=":material/dragon:")
