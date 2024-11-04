023c8ece81861abc0af7bed417f566bac67782c5
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-04 18:43:49 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -7,6 +7,17 @@ def labs(s):
     cc[2].divider()
 st.markdown("# test ❄️")
 st.sidebar.markdown("# test ❄️")
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+with tab2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+with tab3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 st.title('Counter Example using Callbacks')
 if 'count' not in st.session_state:
     st.session_state.count = 0
