65a0375720515addab4d884dfc3aff37d7ad4471
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-04 19:10:50 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -58,14 +58,14 @@ with tab6:
              st.session_state.vote = {"item": item, "reason": reason}
              st.rerun()
         
-        if "vote" not in st.session_state:
-            st.write("Vote for your favorite")
-            if st.button("A"):
-                vote("A")
-            if st.button("B"):
-                vote("B")
-        else:
-            f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
+    if "vote" not in st.session_state:
+        st.write("Vote for your favorite")
+        if st.button("A"):
+            vote("A")
+        if st.button("B"):
+            vote("B")
+     else:
+        f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
     #st.button('dialog',on_click=tvote)       
 #--------------------------------------------------------    
 st.divider()
