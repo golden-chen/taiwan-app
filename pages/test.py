5463812e0084fe2f9c8473b8cd92b1e01f641100
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-04 22:37:43 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -127,4 +127,4 @@ with tab9:
         cols[0].text_input("Name", key="name")
         cols[1].number_input("BETA",key='b11')
         cols[2].form_submit_button("I'm here!", on_click=take_attendance)
-    st.number_input("Alpha",key='a11')
+    st.number_input("Alpha",value=77,key='a11')
